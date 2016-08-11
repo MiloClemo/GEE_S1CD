7f5b903adc5dcf2b9379b53ded306ecd9aa26672
fmcclean <M1nutes158!>
2016-08-11 14:51:11 +0100

    created Aoi class with methods and downloading chunk-wise from server

--- GEE_FloodSAR.py
+++ GEE_FloodSAR.py
@@ -1,49 +1,93 @@
-# Import Modules
-import ee, datetime
+import ee
+import datetime
+import urllib2
+import zipfile
+import requests
+import zipfile
+import StringIO
+
 # Check User Credentials
 ee.Initialize()
-# Load in AOI Fusion Table
-ft2 = ee.FeatureCollection("ft:137bhlHOilFr5iTTuSMWJo7jRLCnSCHQaICzLzmJQ")
-# Load in Flood Scene
-flood = ee.Image('COPERNICUS/S1_GRD/S1A_IW_GRDH_1SDV_20160103T062204_20160103T062229_009326_00D7AC_C9F2')
-# Determine Orbit/Track Metadata
-im_meta1 = flood.getInfo()
-im_meta2 = im_meta1['properties']
-direction = im_meta2['orbitProperties_pass']
-orbit = im_meta2['relativeOrbitNumber_start']
-print 'Orbit Direction: ', direction
-print 'Track Number: ', orbit
+
 # Load in Sentinel-1 Collection
 collection = ee.ImageCollection("COPERNICUS/S1_GRD")
-# Filter S1 Collection
-filter = collection.filterDate(datetime.datetime(2015,7,3), datetime.datetime(2015,11,5)).filterBounds(ft2)\
-    .filter(ee.Filter().eq('transmitterReceiverPolarisation', 'VH')).filter(ee.Filter().eq('instrumentMode', 'IW'))\
-    .filter(ee.Filter().eq('orbitProperties_pass', direction)).filter(ee.Filter().eq('relativeOrbitNumber_start', orbit))
-### Be useful to get a number of images in the collection and use in the filter.toList() command
-### Theoretically there could be hundreds of images going into this calculation in the future
-list = filter.toList(100)
-metalist = list.getInfo()
-for image in metalist:
-    props = image['properties']
-    print props['system:index']
-# Create Median reference image
-ref = filter.median()
-# Median 5x5 Speckle Filter on ref and flood images
-ref = ref.focal_median(5, 'circle', 'pixels').select('VH')
-flood = flood.focal_median(5, 'circle', 'pixels').select('VH')
-# Clip images to AOI
-ref = ref.clip(ft2)
-flood = flood.clip(ft2)
-# Calculate Diff
-diff = flood.select('VH').subtract(ref.select('VH'))
-### Download Image - still needs work
-### Naming download zip file to something more sensible
-### Make so tif downloaded is only AOI or set NoData value not to be 0
-### Increase potential download size to 10x10 pixel spacing can be downloaded
-path = diff.getDownloadUrl({
-    'scale': 30,
-    'description' : 'Jan3VH_diff',
-    #'region' : ft2
-    #'maxPixels' : 1000000000
-})
-print path
+
+
+class Aoi:
+    def __init__(self, fusion_table, flood_scene):
+        self.fusion_table = ee.FeatureCollection(fusion_table)
+        self.flood_scene = ee.Image(flood_scene)
+
+        # Get direction and orbit from metadata
+        self.metadata = self.flood_scene.getInfo()
+        self.properties = self.metadata['properties']
+        self.direction = self.properties['orbitProperties_pass']
+        self.orbit = self.properties['relativeOrbitNumber_start']
+
+        # Filter S1 Collection
+        self.filter = collection.filterDate(datetime.datetime(2015, 7, 3), datetime.datetime(2015, 11, 5))\
+            .filterBounds(self.fusion_table)\
+            .filter(ee.Filter().eq('transmitterReceiverPolarisation', 'VH'))\
+            .filter(ee.Filter().eq('instrumentMode', 'IW'))\
+            .filter(ee.Filter().eq('orbitProperties_pass', self.direction))\
+            .filter(ee.Filter().eq('relativeOrbitNumber_start', self.orbit))
+
+        self.filter_list = self.filter.toList(100)
+        self.meta_list = self.filter_list.getInfo()
+
+        self.results = None
+
+    def print_orbit_and_track(self):
+
+        print 'Orbit Direction: ', self.direction
+        print 'Track Number: ', self.orbit
+
+    def print_filter_metadata(self):
+        for image in self.meta_list:
+            props = image['properties']
+            print props['system:index']
+
+    def analyse(self):
+        # Create Median reference image
+        median_reference_image = self.filter.median()
+
+        # Median 5x5 Speckle Filter on ref and flood images
+        focal_median = median_reference_image.focal_median(5, 'circle', 'pixels').select('VH')
+        flood = self.flood_scene.focal_median(5, 'circle', 'pixels').select('VH')
+
+        # Clip images to AOI
+        clipped_median_image = focal_median.clip(self.fusion_table)
+        clipped_flood_image = flood.clip(self.fusion_table)
+
+        # Calculate Diff
+        self.results = clipped_flood_image.select('VH').subtract(clipped_median_image.select('VH'))
+
+    def download(self):
+
+        path = self.results.getDownloadUrl({
+            'scale': 30,
+            'description' : 'Jan3VH_diff',
+        })
+        print path
+
+        request = requests.get(path, stream=True)
+
+        with open('test.zip', 'wb') as f:
+            for chunk in request.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+
+        zipped_results = zipfile.ZipFile('test.zip')
+        zipped_results.extractall()
+
+
+
+
+if __name__ == '__main__':
+    run = Aoi('ft:137bhlHOilFr5iTTuSMWJo7jRLCnSCHQaICzLzmJQ',
+              'COPERNICUS/S1_GRD/S1A_IW_GRDH_1SDV_20160103T062204_20160103T062229_009326_00D7AC_C9F2')
+
+    run.print_orbit_and_track()
+    run.analyse()
+    run.download()
+
